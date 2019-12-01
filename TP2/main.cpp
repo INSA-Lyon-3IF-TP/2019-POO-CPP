91b3fc5563ab85eab2d4e0280c6b2553a615db1b
Nicolas TROUIN <nicolas.trouin@insa-lyon.fr>
2019-12-01 18:18:46 +0100

    Ajout Menu - Bug sur les ajouts de trajets (#3)

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -11,36 +11,41 @@ void AfficherTrajet(Catalogue & catalogue);
 
 int main()
 {
-  int commande = 0;
-  Catalogue catalogue = Catalogue();
-  cout << "Bienvenue dans ce simulateur de gesyion de transports." << endl << endl;
-  while(commande != -1)
+    int commande = 0;
+    Catalogue catalogue = Catalogue();
+    cout << "Bienvenue dans ce simulateur de gesyion de transports." << endl << endl;
+    while(commande != -1)
     {
-      cout << "Menu :" << endl;
-      cout << "-- (1) Ajouter un trajet au catalogue :" << endl;
-      cout << "-- (2) Afficher le catalogue :" << endl;
-      cout << "-- (3) Rechercher un parcours dans le catalogue :" << endl;
-      cin >> commande;
-      switch(commande)
-	{
-	case 1:
-	  AjouterTrajet(catalogue);
-	  break;
-	case 2:
-	  AfficherTrajet(catalogue);
-	  break;
-	case 3:
-	  RechercherTrajet(catalogue);
-	  break;
-	case -1:
-	  break;
-	default:
-	  cout << "Commande inconnue." << endl;
-	  break;
-	}
-	}
+        cout << "Menu :" << endl
+             << "-- (1) Ajouter un trajet au catalogue :" << endl
+             << "-- (2) Afficher le catalogue :" << endl
+             << "-- (3) Rechercher un parcours dans le catalogue :" << endl
+             << "-- (4) Sortir" << endl;
+        cin >> commande;
+        switch(commande)
+        {
+        case 1:
+            AjouterTrajet(catalogue);
+            catalogue.Afficher();
+            break;
+        case 2:
+            AfficherTrajet(catalogue);
+            break;
+        case 3:
+            RechercherTrajet(catalogue);
+            break;
+        case 4:
+            cout << "Fermeture ..." << endl;
+            return 0;
+        case -1:
+            break;
+        default:
+            cout << "Commande inconnue." << endl;
+            break;
+        }
+    }
 
-  /*Catalogue catalogue = Catalogue();
+    /*Catalogue catalogue = Catalogue();
     TrajetSimple* TS1 = new TrajetSimple("Lyon", "Bordeaux", MoyenTransport::TRAIN); // A CHECK SANS LE NEW ET L'ETOILE
     TrajetSimple* TS2 = new TrajetSimple("Lyon", "Bordeaux", MoyenTransport::AVION);
     TrajetSimple* TC3_1 = new TrajetSimple("Paris", "Marseille", MoyenTransport::AUTO);
@@ -80,36 +85,146 @@ int main()
 
 void RechercherTrajet(Catalogue & catalogue)
 {
-  char villeDep[100],villeArr[100];
-  cout << "Ville de départ : ";
-  cin >> villeDep;
-  cout << "Ville d'arrivée : ";
-  cin >> villeArr;
-  catalogue.Rechercher(villeDep,villeArr);
-  cout << endl << "Résultat de la recherche :" <<endl;
-  catalogue.AfficherResRecherche();
-  cout << endl;
+    char villeDep[100],villeArr[100];
+    cout << "Ville de départ : ";
+    cin >> villeDep;
+    cout << "Ville d'arrivée : ";
+    cin >> villeArr;
+    catalogue.Rechercher(villeDep,villeArr);
+    cout << endl << "Résultat de la recherche :" <<endl;
+    catalogue.AfficherResRecherche();
+    cout << endl;
+}
+
+void AjouterTrajetSimple(Catalogue & catalogue)
+{
+    char villeDep[40];
+    char villeArr[40];
+    unsigned idMoyenTransport;
+    cout << "Veuillez saisir la ville de départ" << endl;
+    cin >> villeDep;
+    cout << "Veuillez saisir la ville d'arrivée" << endl;
+    cin >> villeArr;
+    cout << "Veuillez choisir le moyen de transport" << endl;
+    cout << "(1) AUTO" << endl
+         << "(2) TRAIN" << endl
+         << "(3) AVION" << endl
+         << "(4) BATEAU" << endl;
+    cin >> idMoyenTransport;
+    TrajetSimple* trajetSimple;
+    switch (idMoyenTransport) {
+    case 1:
+        trajetSimple = new TrajetSimple("villeDep", "villeArr", MoyenTransport::AUTO);
+        break;
+    case 2:
+        trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::TRAIN);
+        break;
+    case 3:
+        trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::AVION);
+        break;
+    case 4:
+        trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::BATEAU);
+        break;
+    default:
+        cout << "Erreur" << endl;
+        return;
+    }
+    catalogue.Ajouter(trajetSimple);
+}
+
+void AjouterTrajetComplexe(Catalogue & catalogue)
+{
+    unsigned nbSousTrajets(0);
+    while(nbSousTrajets > 0 && nbSousTrajets < 10)
+    {
+        cout << "De combien de sous-trajets se compose votre trajet complexe ? (entre 1 et 9)" << endl;
+        cin >> nbSousTrajets;
+    }
+    char villeDepGlobal[40];
+    char villeArrGlobal[40];
+    cout << "Veuillez saisir la ville de départ du trajet complexe" << endl;
+    cin >> villeDepGlobal;
+    cout << "Veuillez saisir la ville d'arrivée du trajet complexe" << endl;
+    cin >> villeArrGlobal;
+    const Trajet* liste[9];
+    char villeDep[40];
+    char villeArr[40];
+    unsigned idMoyenTransport;
+    for(unsigned i(0); i < nbSousTrajets; ++i)
+    {
+        cout << "Veuillez saisir la ville de départ" << endl;
+        cin >> villeDep;
+        cout << "Veuillez saisir la ville d'arrivée" << endl;
+        cin >> villeArr;
+        cout << "Veuillez choisir le moyen de transport" << endl;
+        cout << "(1) AUTO" << endl
+             << "(2) TRAIN" << endl
+             << "(3) AVION" << endl
+             << "(4) BATEAU" << endl;
+        cin >> idMoyenTransport;
+        TrajetSimple* trajetSimple;
+        switch (idMoyenTransport) {
+        case 1:
+            trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::AUTO);
+            break;
+        case 2:
+            trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::TRAIN);
+            break;
+        case 3:
+            trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::AVION);
+            break;
+        case 4:
+            trajetSimple = new TrajetSimple(villeDep, villeArr, MoyenTransport::BATEAU);
+            break;
+        default:
+            cout << "Erreur" << endl;
+            return;
+        }
+        liste[0] = trajetSimple;
+    }
+    if(TrajetComplexe::listeCorrect(liste, nbSousTrajets, villeDepGlobal, villeArrGlobal))
+    {
+        TrajetComplexe* trajetComplexe = new TrajetComplexe(liste, nbSousTrajets, villeDepGlobal, villeArrGlobal);
+        catalogue.Ajouter(trajetComplexe);
+    }
 }
 
 void AjouterTrajet(Catalogue & catalogue)
 {
-  TrajetSimple* TS1 = new TrajetSimple("Lyon", "Bordeaux", MoyenTransport::TRAIN);
-  TrajetSimple* TS2 = new TrajetSimple("Lyon", "Bordeaux", MoyenTransport::AVION);
-  TrajetSimple* TC3_1 = new TrajetSimple("Paris", "Marseille", MoyenTransport::AUTO);
-  TrajetSimple* TC3_2 = new TrajetSimple("Marseille", "Lyon", MoyenTransport::BATEAU);
-  catalogue.Ajouter(TS1);
-  catalogue.Ajouter(TS2);
-  const Trajet* liste[2]; // = {TC3_1, TC3_2};
-  liste[0] = TC3_1;
-  liste[1] = TC3_2;
-  if(TrajetComplexe::listeCorrect(liste, 2, "Paris", "Lyon"))
+    cout << "(1) Trajet Simple" << endl
+         << "(2) Trajet Complexe" << endl;
+    int commande(0);
+    cin >> commande;
+    switch (commande) {
+    case 1:
+        AjouterTrajetSimple(catalogue);
+        break;
+    case 2:
+        AjouterTrajetComplexe(catalogue);
+        break;
+    default:
+        cout << "Erreur" << endl;
+        return;
+
+        /*
+    TrajetSimple* TS1 = new TrajetSimple("Lyon", "Bordeaux", MoyenTransport::TRAIN);
+    TrajetSimple* TS2 = new TrajetSimple("Lyon", "Bordeaux", MoyenTransport::AVION);
+    TrajetSimple* TC3_1 = new TrajetSimple("Paris", "Marseille", MoyenTransport::AUTO);
+    TrajetSimple* TC3_2 = new TrajetSimple("Marseille", "Lyon", MoyenTransport::BATEAU);
+    catalogue.Ajouter(TS1);
+    catalogue.Ajouter(TS2);
+    const Trajet* liste[2]; // = {TC3_1, TC3_2};
+    liste[0] = TC3_1;
+    liste[1] = TC3_2;
+    if(TrajetComplexe::listeCorrect(liste, 2, "Paris", "Lyon"))
     {
-      TrajetComplexe* TC3 = new TrajetComplexe(liste, 2, "Paris", "Lyon");
-      catalogue.Ajouter(TC3);
+        TrajetComplexe* TC3 = new TrajetComplexe(liste, 2, "Paris", "Lyon");
+        catalogue.Ajouter(TC3);
+    }
+    */
     }
 }
-
 void AfficherTrajet(Catalogue & catalogue)
 {
-  catalogue.Afficher();
+    catalogue.Afficher();
 }
